3614569bd7e55ac7ffd3fbca8a3f271a2d8d3d7b
KUROKI Masayuki <m_kuroki@mac.com>
2013-06-22 15:39:56 +0900

    Add Umbrella Header.

---
+++ PgTest/Classes/PgSQL.h
@@ -0,0 +1,30 @@
+//
+//  PgSQL.h
+//  PgTest
+//
+//  Created by 黒木 政幸 on 2013/06/22.
+//
+//
+
+#ifndef PgTest_PgSQL_h
+#define PgTest_PgSQL_h
+
+#include "PgSQL.h"
+#include "PgSQLCoder.h"
+#include "PgSQLCommand.h"
+#include "PgSQLConnection.h"
+#include "PgSQLConnectionInfo.h"
+#include "PgSQLContext.h"
+#include "PgSQLDelete.h"
+#include "PgSQLInsert.h"
+#include "PgSQLQuery.h"
+#include "PgSQLRecord.h"
+#include "PgSQLRelationship.h"
+#include "PgSQLResult.h"
+#include "PgSQLSingleCommand.h"
+#include "PgSQLTransaction.h"
+#include "PgSQLUpdate.h"
+#include "PgSQLValue.h"
+#include "PgSQLWhereCommand.h"
+
+#endif
